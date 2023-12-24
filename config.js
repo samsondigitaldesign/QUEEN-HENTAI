a8f2f73dafcedffca8de248e5f3b4256ecf5cc34
samsondigitaldesign <124357890+samsondigitaldesign@users.noreply.github.com>
2023-12-24 06:05:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['2349155610431', 'Dinuwa Official²⁰²³', true],
+  ['2349155610431', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
